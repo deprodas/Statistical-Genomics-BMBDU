252613aa9c2abce8d36ba31004d1abfdb897382e
Depro Das <deprodasprotay@gmail.com>
2023-01-11 23:45:57 +0600

    Initial commit version control

--- microarray_initials.R
+++ microarray_initials.R
@@ -24,24 +24,24 @@ library(hugene10sttranscriptcluster.db)
 
 # Raw data input & Background correction, Normalization, Calculating Expression 
 
-rawdata_1 <- ReadAffy() 
-Biobase::exprs(rawdata_1) 
+rawdata <- ReadAffy() 
+Biobase::exprs(rawdata) 
 
 normalized_data <- oligo::rma(raw_data, target = "core")  
 normalized_express_data <- as.data.frame(exprs(normalized_data))
 
 # Map probe IDs to gene symbols 
 
-anno_palmieri <- AnnotationDbi::select(hugene10sttranscriptcluster.db,
-                                       keys = (featureNames(normalized_data)),
-                                       columns = c("SYMBOL", "GENENAME"),
-                                       keytype = "PROBEID") 
+anno_primary <- AnnotationDbi::select(hugene10sttranscriptcluster.db,
+                                      keys = (featureNames(normalized_data)),
+                                      columns = c("SYMBOL", "GENENAME"),
+                                      keytype = "PROBEID") 
 
-anno_palmieri <- subset(anno_palmieri, !is.na(SYMBOL)) 
+anno_primary <- subset(anno_primary, !is.na(SYMBOL)) 
 
 # Remove multiple mappings (probe IDs) from the assay data  
 
-anno_grouped <- group_by(anno_palmieri, PROBEID)
+anno_grouped <- group_by(anno_primary, PROBEID)
 anno_summarized <- dplyr::summarize(anno_grouped, no_of_matches = n_distinct(SYMBOL))
 anno_filtered <- filter(anno_summarized, no_of_matches > 1) 
 head(anno_filtered) 
@@ -60,17 +60,17 @@ validObject(normalized_final)
 
 # Selection  
 
-all(normalized_express_final$PROBEID %in% anno_palmieri$PROBEID) 
-normalized_express_final$PROBEID %in% anno_palmieri$PROBEID
+all(normalized_express_final$PROBEID %in% anno_primary$PROBEID) 
+normalized_express_final$PROBEID %in% anno_primary$PROBEID
 class(normalized_express_final$PROBEID) 
-class(anno_palmieri$PROBEID) 
+class(anno_primary$PROBEID) 
 
-all(anno_palmieri$PROBEID %in% normalized_express_final$PROBEID) 
-anno_palmieri$PROBEID %in% normalized_express_final$PROBEID
-class(anno_palmieri)
+all(anno_primary$PROBEID %in% normalized_express_final$PROBEID) 
+anno_primary$PROBEID %in% normalized_express_final$PROBEID
+class(anno_primary)
 class(normalized_express_final)
 
-Joined_df <- dplyr::left_join(anno_palmieri, normalized_express_final, by= c("PROBEID"= "PROBEID"))
+Joined_df <- dplyr::left_join(anno_primary, normalized_express_final, by= c("PROBEID"= "PROBEID"))
 
 Joined_norExp_ID <- na.omit(Joined_df) 
 
